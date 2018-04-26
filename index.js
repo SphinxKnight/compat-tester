e659648db9b57326db68f21783aa4ccba8795a1c
SphinxKnight <SphinxKnight@users.noreply.github.com>
2018-04-26 20:17:21 +0200

    Piping options to module methods

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const htmlAnalyzer = require("./src/html/compat-analyzer");
 const reportHelpers = require("./lib/report");
 
 // Exports API
-exports.htmlStringAnalyzer = function htmlStringAnalyzer (string, browserScope){
+exports.htmlStringAnalyzer = function htmlStringAnalyzer (string, browserScope, options){
     htmlAnalyzer.analyzeString(string, browserScope, 0, "HTMLString", (e, d) => {
         if (e) {
             console.error(e);
@@ -18,10 +18,10 @@ exports.htmlStringAnalyzer = function htmlStringAnalyzer (string, browserScope){
         console.log("HTML Report:");
         report.sort(reportHelpers.sortReport);
         report.map(reportHelpers.printReportLine);
-    });
+    }, options);
 };
 
-exports.cssStringAnalyzer = function cssStringAnalyzer (string, browserScope){
+exports.cssStringAnalyzer = function cssStringAnalyzer (string, browserScope, options){
     cssAnalyzer.analyzeString(string, browserScope, 0, "CSSString", (e, d) => {
         if (e) {
             console.error(e);
@@ -31,10 +31,10 @@ exports.cssStringAnalyzer = function cssStringAnalyzer (string, browserScope){
         console.log("CSS Report:");
         report.sort(reportHelpers.sortReport);
         report.map(reportHelpers.printReportLine);
-    });
+    }, options);
 };
 
-exports.jsStringAnalyzer = function jsStringAnalyzer (string, browserScope){
+exports.jsStringAnalyzer = function jsStringAnalyzer (string, browserScope, options){
     jsAnalyzer.analyzeString(string, browserScope, 0, "JSString", (e, d) => {
         if (e) {
             console.error(e);
@@ -44,10 +44,10 @@ exports.jsStringAnalyzer = function jsStringAnalyzer (string, browserScope){
         console.log("JS Report:");
         report.sort(reportHelpers.sortReport);
         report.map(reportHelpers.printReportLine);
-    });
+    }, options);
 };
 
-exports.htmlFileAnalyzer = function htmlFileAnalyzer (filePath, browserScope){
+exports.htmlFileAnalyzer = function htmlFileAnalyzer (filePath, browserScope, options){
     htmlAnalyzer.analyzeFile(filePath, browserScope, (e, d) => {
         if (e) {
             console.error(e);
@@ -57,10 +57,10 @@ exports.htmlFileAnalyzer = function htmlFileAnalyzer (filePath, browserScope){
         console.log("HTML Report:");
         report.sort(reportHelpers.sortReport);
         report.map(reportHelpers.printReportLine);
-    });
+    }, options);
 };
 
-exports.cssFileAnalyzer = function cssFileAnalyzer (filePath, browserScope){
+exports.cssFileAnalyzer = function cssFileAnalyzer (filePath, browserScope, options){
     const content = fs.readFileSync(filePath,"utf-8");
     cssAnalyzer.analyzeString(content, browserScope, 0, path.basename(filePath), (e, d) => {
         if (e) {
@@ -71,10 +71,10 @@ exports.cssFileAnalyzer = function cssFileAnalyzer (filePath, browserScope){
         console.log("CSS Report:");
         report.sort(reportHelpers.sortReport);
         report.map(reportHelpers.printReportLine);
-    });
+    }, options);
 };
 
-exports.jsFileAnalyzer = function jsFileAnalyzer (filePath, browserScope){
+exports.jsFileAnalyzer = function jsFileAnalyzer (filePath, browserScope, options){
     const content = fs.readFileSync(filePath,"utf-8");
     jsAnalyzer.analyzeString(content, browserScope, 0, path.basename(filePath), (e, d) => {
         if (e) {
@@ -85,5 +85,5 @@ exports.jsFileAnalyzer = function jsFileAnalyzer (filePath, browserScope){
         console.log("JavaScript analysis isn't yet implemented - JS Report:");
         report.sort(reportHelpers.sortReport);
         report.map(reportHelpers.printReportLine);
-    });
+    }, options);
 };
